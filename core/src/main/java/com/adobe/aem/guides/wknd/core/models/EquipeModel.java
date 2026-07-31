4d2e63ddf016813c2a4ef748fa00d3e98faf4cd9
AntonniSMoraes <antonnimoraes@gmail.com>
2026-07-30 21:07:57 -0300

    feat: componente editado e concluído conforme checklist do desafio

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/EquipeModel.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/EquipeModel.java
@@ -1,32 +1,59 @@
 package com.adobe.aem.guides.wknd.core.models;
 
+import com.adobe.aem.guides.wknd.core.services.MostrarEquipeService;
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-@Model(adaptables = Resource.class)
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Model(
+    adaptables = {SlingHttpServletRequest.class, Resource.class},
+    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
 public class EquipeModel {
 
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String nome;
+    @ValueMapValue
+    @Default(values = "Nossa Equipe")
+    private String titulo;
 
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String cargo;
+    @ChildResource
+    private List<Resource> membros;
 
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String urlImagem;
+    @OSGiService
+    private MostrarEquipeService mostrarEquipeService;
 
+    private List<Resource> membrosExibidos;
 
-    public String getNome() {
-        return nome;
+    @PostConstruct
+    protected void init() {
+        if (membros != null && !membros.isEmpty()) {
+            int limite = (mostrarEquipeService != null) ? mostrarEquipeService.getMaxMembros() : membros.size();
+            this.membrosExibidos = membros.stream()
+                    .limit(limite)
+                    .collect(Collectors.toList());
+        } else {
+            this.membrosExibidos = Collections.emptyList();
+        }
     }
-    
-    public String getCargo() {
-        return cargo;
+
+    public String getTitulo() {
+        return titulo;
     }
 
-    public String getUrlImagem() {
-        return urlImagem;
+    public List<Resource> getMembros() {
+        return membrosExibidos;
+    }
+
+    public boolean isHasMembros() {
+        return !membrosExibidos.isEmpty();
     }
 }
